30e9e7ca5575439d35cabfeb093d7d6116221963
Hoffnis <50756806+Hoffnis@users.noreply.github.com>
2026-07-14 13:47:38 -0300

    Update task.spec.ts

--- task-manager-frontend/src/app/services/task.spec.ts
+++ task-manager-frontend/src/app/services/task.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-// 1. Corrija o import para buscar a classe TaskService
 import { TaskService } from './task';
 
 describe('TaskService', () => {
@@ -10,14 +9,14 @@ describe('TaskService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      // Como o TaskService usa o HttpClient, precisamos fornecer os providers de teste aqui
+
       providers: [
         TaskService,
         provideHttpClient(),
         provideHttpClientTesting()
       ]
     });
-    // 2. Injete a classe correta
+
     service = TestBed.inject(TaskService);
   });
 
